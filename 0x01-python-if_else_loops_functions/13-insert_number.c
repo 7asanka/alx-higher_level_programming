8d42d5f3d3586acbd600871ba8cb423de0a34de6
7asanka <hassan2002_kimo@hotmail.com>
2024-09-10 12:53:08 -0700

    0x01 task 13 fix

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -25,10 +25,11 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 
-	while (current->next != NULL && current->next->n >= number)
+	while (current->next != NULL && current->next->n < number)
 	{
 		current = current->next;
 	}
+
 	new->next = current->next;
 	current->next = new;
 
