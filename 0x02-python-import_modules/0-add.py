87b5864615b4120e3d84ec6eab782e56aa57139c
7asanka <hassan2002_kimo@hotmail.com>
2024-09-12 07:38:39 -0700

    0x02 task 0 fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == "__main__":
+    import add_0
 
-add = add_0.add
-a = 1
-b = 2
-print("{} + {} = {res}".format(a, b, res=add(a, b)))
+    add = add_0.add
+    a = 1
+    b = 2
+    print("{} + {} = {res}".format(a, b, res=add(a, b)))
