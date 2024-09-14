e16e3ba7c2136462ece5cf8877af628035444274
7asanka <hassan2002_kimo@hotmail.com>
2024-09-14 10:03:00 -0700

    0x03 tasks fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     cpy_list = [i for i in my_list]
     if idx < 0:
         return cpy_list
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return cpy_list
 
     cpy_list[idx] = element
