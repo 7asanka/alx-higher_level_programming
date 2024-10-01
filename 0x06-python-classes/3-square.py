738881e8b3502f7476dec86a9ede9f1ade0d9142
7asanka <hassan2002_kimo@hotmail.com>
2024-10-01 10:25:30 -0700

    0x06 task 3 added

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module defines a square class"""
+
+
+class Square:
+    """
+    this is a square class
+
+    Attributes:
+        __size: the sie of the square
+
+    methods:
+        __init__: initializes the class
+    """
+
+    def __init__(self, size=0):
+        """
+        initializes a square
+
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
+
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        """
+        calculates the area of the square
+
+        Returns:
+            the area of the square (size x size)
+        """
+        return self.__size * self.__size
