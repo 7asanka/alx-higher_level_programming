17628b6b701b5c205b6620ebf7d59bc71a67c0be
7asanka <hassan2002_kimo@hotmail.com>
2024-10-01 09:00:19 -0700

    0x06 task 0 test

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """ this is a square class
+    """ 
+    this is a square class
+
+    methods:
+        __init__: initialises the class
     """
+
+    def __init__(self):
+        pass
