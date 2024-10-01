2b801b47d9664141db283686004ddc743d3e68fb
7asanka <hassan2002_kimo@hotmail.com>
2024-10-01 09:16:32 -0700

    0x06 task 1 added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a square class"""
+
+
+class Square:
+    """
+    this is a square class
+
+    Attributes:
+        __size: the sie of the square
+
+    methods:
+        __init__: initializes the class
+    """
+
+    def __init__(self, size):
+        """
+        initializes a square
+
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
