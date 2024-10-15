2206745be7b49b9547fe53543a46e1ab5ad20b97
7asanka <hassan2002_kimo@hotmail.com>
2024-10-15 10:42:24 -0700

    0x0B task 10 added

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines a student class
+"""
+
+
+class Student():
+    """
+    Student class
+
+    attributes:
+        first_name: first name
+        last_name: last name
+        age: age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=""):
+        """that retrieves a dictionary representation of a Student instance"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
