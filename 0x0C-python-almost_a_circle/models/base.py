831486ecdcffbaa07a93cc7e4d6f66172a988068
7asanka <hassan2002_kimo@hotmail.com>
2024-10-17 11:49:35 -0700

    0x0C added models/ base rectangle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module defines a class representing the base class
+"""
+
+
+class Base:
+    """
+    class representing the base class
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        initializes a new base
+
+        args:
+            id: new base id
+        """
+
+        if id not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+This module definse a class representing a rectangle
+"""
+
+
+class Rectangle:
+    """
+    a class representing a rectangle
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a new Rectangle
+
+        args:
+            width: width of the rectangle
+            height: heigt of the rectangle
+            x:
+            y:
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        self.__y = value
