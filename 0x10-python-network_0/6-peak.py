31ba54cd726e31a9b02ade4fcddd32b6bc8c94d3
7asanka <hassan2002_kimo@hotmail.com>
2025-01-16 12:29:37 -0800

    0x10 task 6 added

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+def find_peak(list_of_integers):
+    """Finds a peak element in a list of unsorted integers."""
+    if not list_of_integers:
+        return None
+
+    def find_peak_recursive(nums, left, right):
+        """Helper function to find a peak using divide-and-conquer."""
+        mid = (left + right) // 2
+
+        if (mid == 0 or nums[mid] >= nums[mid - 1]) and \
+           (mid == len(nums) - 1 or nums[mid] >= nums[mid + 1]):
+            return nums[mid]
+
+        if mid > 0 and nums[mid - 1] > nums[mid]:
+            return find_peak_recursive(nums, left, mid - 1)
+
+        return find_peak_recursive(nums, mid + 1, right)
+
+    return find_peak_recursive(list_of_integers, 0, len(list_of_integers) - 1)
